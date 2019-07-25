29439c9541a3f7c2f9977f364ba0b18a09e92edd
adrian7123 <bueno1dad@gmail.com>
2019-07-25 00:17:45 -0300

    upload 25/07

---
+++ site/js/TELA.js
@@ -0,0 +1,18 @@
+//.  VARIAVEIS DA TELA 
+
+var ALTURA = innerHeight,
+	LARGURA = innerWidth;
+	
+//alert(LARGURA)
+	
+	
+if(LARGURA < 530){
+
+	
+}else {
+	console.log('sem alterações no momento')
+}
+
+
+
+
